25e1eb0ab0a08b9385772381e3739d4a92275fcb
lingjian99 <lingjian99@yeah.net>
2021-11-09 20:37:21 -0800

    first commit

---
+++ JS/blog_chief.js
@@ -0,0 +1,17 @@
+
+function blog_chief_large(title, size)
+{
+    let str ="<div class=\"col-sm-"
+        + size
+        + " dash\"><div class=\"row\"><div class=\"col-md-12 dash1\"><a href=\"./blogs/"
+        + title
+        + "/main.html\"><img src=\"./blogs/"
+        + title
+        + "/images/cover.jpg\" class=\"img-responsive\" alt=\"No image\"></img></a></div></div></div>";
+    document.write(str);
+}
+
+function blog_chief_small()
+{
+
+}
