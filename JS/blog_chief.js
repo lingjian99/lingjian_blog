20f346a740073424a2f6f1924f13f77470e944ff
lingjian99 <lingjian99@yeah.net>
2022-03-03 19:17:12 -0800

    整理

--- JS/blog_chief.js
+++ JS/blog_chief.js
@@ -1,17 +1,12 @@
 
 function blog_chief_large(title, size)
 {
-    let str ="<div class=\"col-sm-"
-        + size
-        + " dash\"><div class=\"row\"><div class=\"col-md-12 dash1\"><a href=\"./blogs/"
+    let str ="<div class=\"div1 col-lg-3"
+
+        + " col-md-4 col-sm-6 col-xs-12 dash\"><div class=\"row\"><div><a href=\"./blogs/"
         + title
-        + "/main.html\"><img src=\"./blogs/"
+        + "/main.html\"><img class=\"dash\" src=\"./blogs/"
         + title
         + "/images/cover.jpg\" class=\"img-responsive\" alt=\"No image\"></img></a></div></div></div>";
     document.write(str);
-}
-
-function blog_chief_small()
-{
-
 }
